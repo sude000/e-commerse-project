549a0a5c128d38cf8bdbb87bf19f614b9f2c92ae
Sude Guler <sudegulerrss@gmail.com>
2024-02-21 08:20:08 +0300

    Guzelimmmm

--- Odev2.3/obj/Debug/net8.0/Odev2.3.AssemblyInfo.cs
+++ Odev2.3/obj/Debug/net8.0/Odev2.3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Odev2.3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fc334f14493720e4504e5e21adffa1e6ae63112")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602a8f59fc67529d83287cef02e17c9f9caed9bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Odev2.3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Odev2.3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
